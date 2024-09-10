5c938e5ecbb394290f5dbe23052a4025c9b9c5b6
JavzaaBatmunkh <javzaabatmunkh@gmail.com>
2024-09-10 15:37:33 +0800

    styling

--- expense-tracker-web/app/page.js
+++ expense-tracker-web/app/page.js
@@ -243,133 +243,169 @@ export default function Home() {
     }
   }, [categoryId, filterType])
 
-
   return (
-    <main className="flex gap-10  bg-slate-200 w-full px-[5%] py-8">
-      <RecordDialog onComplete={loadTransactions} />
-      <Toaster richColors />
-      <Card className="bg-slate-100">
-        <CardHeader>
-          <CardTitle>Records</CardTitle>
-          <Button onClick={() => router.push(`?create=new`)}>+Add</Button>
-        </CardHeader>
-        <CardContent>
-          <p className='font-bold py-4'>Types</p>
-          <RadioGroup value={filterType} onValueChange={(val) => setFilterType(val)}>
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="ALL" id="option-one" />
-              <Label htmlFor="option-one">All</Label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="INCOME" id="option-two" />
-              <Label htmlFor="option-two">Income</Label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="EXPENSE" id="option-three" />
-              <Label htmlFor="option-three">Expense</Label>
-            </div>
-          </RadioGroup>
-          <p className='font-bold py-4'>Categories</p>
-          {categories.map((category) => (
-            <div key={category.id} className="flex gap-2 mb-1">
-              <Button onClick={() => setEditingCategory(category)} className="bg-slate-400">
-                <Pencil className='w-4' />
-              </Button>
-              <Button onClick={() => deleteCategory(category.id)} className="bg-slate-400">
-                <Trash2 className='w-4' />
-              </Button>
-
-              <div onClick={() => toggleCategoryId(category.id)} className="flex justify-between w-full">
-                <div className='flex gap-2'>
-                  <CategoryIcon iconName={category.icon} color={category.color} />
-                  {category.name}
-                </div>
-                <ArrowBigRightDash />
+    <main>
+      <div className="flex gap-10  bg-slate-200 w-full px-[5%] py-8 hidden">
+        <RecordDialog onComplete={loadTransactions} />
+        <Toaster richColors />
+        <Card className="bg-slate-100">
+          <CardHeader>
+            <CardTitle>Records</CardTitle>
+            <Button onClick={() => router.push(`?create=new`)}>+Add</Button>
+          </CardHeader>
+          <CardContent>
+            <p className='font-bold py-4'>Types</p>
+            <RadioGroup value={filterType} onValueChange={(val) => setFilterType(val)}>
+              <div className="flex items-center space-x-2">
+                <RadioGroupItem value="ALL" id="option-one" />
+                <Label htmlFor="option-one">All</Label>
               </div>
+              <div className="flex items-center space-x-2">
+                <RadioGroupItem value="INCOME" id="option-two" />
+                <Label htmlFor="option-two">Income</Label>
+              </div>
+              <div className="flex items-center space-x-2">
+                <RadioGroupItem value="EXPENSE" id="option-three" />
+                <Label htmlFor="option-three">Expense</Label>
+              </div>
+            </RadioGroup>
+            <p className='font-bold py-4'>Categories</p>
+            {categories.map((category) => (
+              <div key={category.id} className="flex gap-2 mb-1">
+                <Button onClick={() => setEditingCategory(category)} className="bg-slate-400">
+                  <Pencil className='w-4' />
+                </Button>
+                <Button onClick={() => deleteCategory(category.id)} className="bg-slate-400">
+                  <Trash2 className='w-4' />
+                </Button>
 
-            </div>
-          ))}
-          {/* add new category button */}
-          <Button onClick={() => { reset(); setOpen(true) }} variant="outline" className="my-4">
-            + Add New Category
-          </Button>
-          <Dialog open={open}>
-            <DialogContent>
-              <DialogHeader>
-                <DialogTitle>Add Category</DialogTitle>
-                <hr />
-                <DialogDescription className="flex gap-4">
-                  <Popover>
-                    <PopoverTrigger><CategoryIcon iconName={icon} color={color} /></PopoverTrigger>
-                    <PopoverContent >
-                      <div className="grid grid-cols-4 gap-2">
-                        {categoryIcons.map(({ name, Icon }) =>
-                          <div className={`relative w-8 h-8 flex justify-center items-center rounded-lg
+                <div onClick={() => toggleCategoryId(category.id)} className="flex justify-between w-full">
+                  <div className='flex gap-2'>
+                    <CategoryIcon iconName={category.icon} color={category.color} />
+                    {category.name}
+                  </div>
+                  <ArrowBigRightDash />
+                </div>
+
+              </div>
+            ))}
+            {/* add new category button */}
+            <Button onClick={() => { reset(); setOpen(true) }} variant="outline" className="my-4">
+              + Add New Category
+            </Button>
+            <Dialog open={open}>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Add Category</DialogTitle>
+                  <hr />
+                  <DialogDescription className="flex gap-4">
+                    <Popover>
+                      <PopoverTrigger><CategoryIcon iconName={icon} color={color} /></PopoverTrigger>
+                      <PopoverContent >
+                        <div className="grid grid-cols-4 gap-2">
+                          {categoryIcons.map(({ name, Icon }) =>
+                            <div className={`relative w-8 h-8 flex justify-center items-center rounded-lg
                       ${icon === name ? "bg-blue-300 border-blue-950" : ""}`} value={name} key={name}
-                            onClick={() => setIcon(name)}>
-                            {<Icon />}
-                          </div>)}
-                      </div>
-                      <hr className="my-4" />
-                      <div className="flex gap-1">
-                        {categoryColors.map(({ name, value }) =>
-                          <div key={name} className="rounded-full h-8 w-8 flex justify-center items-center" style={{ background: value }}
-                            onClick={() => setColor(name)}>
-                            {color === name && <Check className="text-white w-4" />}
-                          </div>)}
-                      </div>
-                    </PopoverContent>
-                  </Popover>
-                  <Input placeholder="Name" type="text" value={newCategory} disabled={loading}
-                    onChange={(event) => { setNewCategory(event.target.value) }} />
-                </DialogDescription>
-              </DialogHeader>
-              <DialogFooter>
-                {editingCategory ? (
-                  <Button onClick={updateCategory} className="bg-green-700 hover:bg-green-900" disabled={loading}>Update</Button>
-                ) : (
-                  <Button onClick={createNew} className="bg-green-700 hover:bg-green-900" disabled={loading}>Add</Button>
-                )}
-                <Button onClick={() => setOpen(false)}>Cancel</Button>
-              </DialogFooter>
-            </DialogContent>
-          </Dialog>
-        </CardContent>
-        <CardFooter>
-        </CardFooter>
-      </Card>
+                              onClick={() => setIcon(name)}>
+                              {<Icon />}
+                            </div>)}
+                        </div>
+                        <hr className="my-4" />
+                        <div className="flex gap-1">
+                          {categoryColors.map(({ name, value }) =>
+                            <div key={name} className="rounded-full h-8 w-8 flex justify-center items-center" style={{ background: value }}
+                              onClick={() => setColor(name)}>
+                              {color === name && <Check className="text-white w-4" />}
+                            </div>)}
+                        </div>
+                      </PopoverContent>
+                    </Popover>
+                    <Input placeholder="Name" type="text" value={newCategory} disabled={loading}
+                      onChange={(event) => { setNewCategory(event.target.value) }} />
+                  </DialogDescription>
+                </DialogHeader>
+                <DialogFooter>
+                  {editingCategory ? (
+                    <Button onClick={updateCategory} className="bg-green-700 hover:bg-green-900" disabled={loading}>Update</Button>
+                  ) : (
+                    <Button onClick={createNew} className="bg-green-700 hover:bg-green-900" disabled={loading}>Add</Button>
+                  )}
+                  <Button onClick={() => setOpen(false)}>Cancel</Button>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
+          </CardContent>
+          <CardFooter>
+          </CardFooter>
+        </Card>
 
 
-      <div className='w-full'>
-        {transactions.map((transaction) => (
-          <div key={transaction.id}>
-            <p className='font-bold'>{transaction.date.split('T')[0]}</p>
-            <Card className="w-full mb-4 pt-4">
-              <CardContent className="flex justify-between items-center ">
-                <div className='flex items-center gap-2'>
-                  <Checkbox />
-                  <div className="w-10 h-10 rounded-full flex justify-center items-center" style={{ background: transaction.color }}>
-                    <CategoryIcon iconName={transaction.icon} className="text-white" />
+        <div className='w-full'>
+          {transactions.map((transaction) => (
+            <div key={transaction.id}>
+              <p className='font-bold'>{transaction.date.split('T')[0]}</p>
+              <Card className="w-full mb-4 pt-4">
+                <CardContent className="flex justify-between items-center ">
+                  <div className='flex items-center gap-2'>
+                    <Checkbox />
+                    <div className="w-10 h-10 rounded-full flex justify-center items-center" style={{ background: transaction.color }}>
+                      <CategoryIcon iconName={transaction.icon} className="text-white" />
+                    </div>
+                    <div>
+                      <p>{transaction.name}</p>
+                      <p className='text-slate-500'>{transaction.time.split(':')[0]}:{transaction.time.split(':')[1]}</p>
+                    </div>
                   </div>
-                  <div>
-                    <p>{transaction.name}</p>
-                    <p className='text-slate-500'>{transaction.time.split(':')[0]}:{transaction.time.split(':')[1]}</p>
+                  <div className='flex items-center gap-2'>
+                    {transaction.amount}
+                    <Button onClick={() => router.push(`?editing=${transaction.id}`)} className="bg-slate-400">
+                      <Pencil className='w-4' />
+                    </Button>
+                    <Button onClick={() => deleteTransaction(transaction.id)} className="bg-slate-400">
+                      <Trash2 className='w-4' />
+                    </Button>
                   </div>
-                </div>
-                <div className='flex items-center gap-2'>
-                  {transaction.amount}
-                  <Button onClick={() => router.push(`?editing=${transaction.id}`)} className="bg-slate-400">
-                    <Pencil className='w-4' />
-                  </Button>
-                  <Button onClick={() => deleteTransaction(transaction.id)} className="bg-slate-400">
-                    <Trash2 className='w-4' />
-                  </Button>
-                </div>
-              </CardContent>
-            </Card>
-          </div>
+                </CardContent>
+              </Card>
+            </div>
 
-        ))}
+          ))}
+        </div>
+      </div>
+      <div className='px-[5%] bg-slate-200 h-screen pt-4'>
+        <div className='grid grid-cols-3 gap-4'>
+        <Card className="bg-blue">
+            <CardHeader>
+            </CardHeader>
+            <CardFooter>
+              <p>Your Cash</p>
+            </CardFooter>
+          </Card>
+          <Card>
+            <p className='p-4'>Total Income</p>
+            <hr/>
+            <CardHeader>
+              <CardTitle>1200000</CardTitle>
+              <CardDescription>Your Income Amount</CardDescription>
+            </CardHeader>
+            <CardFooter>
+              <p>32% from last month</p>
+            </CardFooter>
+          </Card>
+          <Card>
+            <p className='p-4'>Total Expenses</p>
+            <hr/>
+            <CardHeader>
+              <CardTitle>1200000</CardTitle>
+              <CardDescription>Your Expense Amount</CardDescription>
+            </CardHeader>
+            <CardFooter>
+              <p>32% from last month</p>
+            </CardFooter>
+          </Card>
+
+        </div>
+
       </div>
     </main>
   );
